9a2f82848b27291ce31202f519cf826c125cab48
ajmalesa <ajmalesa@gmail.com>
2020-02-13 19:46:54 -0700

    Added coins feature, with a bit of RNG. Added floating and fading text feature for points gained on each click. And added open game art for coin images

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.clicker;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,19 +20,28 @@ public class MainActivity extends AppCompatActivity {
     private float autoRate;
     private int upgradeCost;
     private int autoUpgradeCost;
+    private int imageId;
 
     Button clickButton;
     Button upgradeButton;
     Button autoUpgradeButton;
     TextView pointsTextView;
     TextView autoTextView;
+    TextView pointsGainedTextView;
 
+    // Used as timer for tasks that run constantly
     Handler handler = new Handler();
+
+    // Used to play sound effects
+    MediaPlayer mp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.coin);
 
         points = 0;
         autoRate = 0;
@@ -40,8 +52,10 @@ public class MainActivity extends AppCompatActivity {
         clickButton = (Button) findViewById(R.id.click_button);
         upgradeButton = (Button) findViewById(R.id.upgrade_button);
         autoUpgradeButton = (Button) findViewById(R.id.auto_upgrade_button);
-        pointsTextView = (TextView) findViewById((R.id.points_textview));
-        autoTextView = (TextView) findViewById((R.id.auto_textview));
+        pointsTextView = (TextView) findViewById(R.id.points_textview);
+        autoTextView = (TextView) findViewById(R.id.auto_textview);
+        pointsGainedTextView = (TextView) findViewById(R.id.points_gained_textview);
+
 
         handler.post(runnableCode);
     }
@@ -54,15 +68,30 @@ public class MainActivity extends AppCompatActivity {
 
             // Run this code when the main button is clicked
             case R.id.click_button:
+                // Add to points value the click value and set it to the points textview
                 pointsTextView = (TextView) findViewById(R.id.points_textview);
                 points += Integer.parseInt(clickButton.getText().toString());
                 pointsTextView.setText(String.valueOf(Math.round(points)));
 
-                // Move main button by translation to randomize where it will be next
+                // Enable visibility of floating points gained text to prepare it for animation
+                pointsGainedTextView.setVisibility(View.VISIBLE);
+
+                mp.start();
+
+                // Move main button by translation to randomize where it will be next and add
+                // floating and fading away text position to show points gained on last position
+                // clicked
                 int randomX = new Random().nextInt(160 + 160) - 160;
                 int randomY = new Random().nextInt(160 + 160) - 160;
+                pointsGainedTextView.setX(clickButton.getX());
+                pointsGainedTextView.setY(clickButton.getY() - 80);
                 clickButton.setTranslationX(randomX);
                 clickButton.setTranslationY(randomY);
+
+                // Start animation and disable visibility of floating text after animation
+                Animation slideUpAnimation = AnimationUtils.loadAnimation(this,R.anim.up_slide_animation);
+                pointsGainedTextView.startAnimation(slideUpAnimation);
+                pointsGainedTextView.setVisibility(View.INVISIBLE);
                 break;
 
             // Run this code when the upgrade button is clicked
@@ -72,7 +101,17 @@ public class MainActivity extends AppCompatActivity {
                     currentAttack *= 1.2;
                     points -= upgradeCost;
                     upgradeCost *= 2;
+
+                    // Set image based on which attack the current upgrade is on, so we
+                    // do not have to hardcode changing the button image for each upgrade
+                    imageId = this.getResources().getIdentifier(
+                            "image"+currentAttack,
+                            "drawable",
+                            this.getPackageName());
+                    clickButton.setBackgroundResource(imageId);
+
                     clickButton.setText(String.valueOf(currentAttack));
+                    pointsGainedTextView.setText("+" + String.valueOf(currentAttack));
                 }
                 break;
 
